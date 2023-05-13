41f1a1ecaf1e364fe9e8a17deaa4a0eb61cc731b
PHAMQUOCHOANG\quoch <quochoang22603>
2023-05-13 19:48:03 +0700

    #23 Tạo Tất Cả Models, Migrations và Seeders Cho Dự Án Đặt Lịch Khám Bệnh - Phần 1

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -5,9 +5,14 @@ module.exports = {
     async up(queryInterface, Sequelize) {
         return queryInterface.bulkInsert('Users', [
             {
-                firstName: 'John',
-                lastName: 'Doe',
-                email: 'example@example.com',
+                email: 'admin@example.com',
+                password: '123456',
+                firstName: 'QuocHoang22603',
+                lastName: '22603',
+                address: 'VietNam',
+                gender: 1,
+                typeRole: 'ROLE',
+                keyRole: 'R1',
                 createdAt: new Date(),
                 updatedAt: new Date(),
             },
